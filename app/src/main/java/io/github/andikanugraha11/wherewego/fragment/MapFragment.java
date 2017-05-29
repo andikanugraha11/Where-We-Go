632f3feb6fd8e8cc9099562bc08d1c3ad91dd195
Andika Nugraha <m.andika.nugraha@gmail.com>
2017-05-29 15:33:13 +0700

    map embeded

--- app/src/main/java/io/github/andikanugraha11/wherewego/fragment/MapFragment.java
+++ app/src/main/java/io/github/andikanugraha11/wherewego/fragment/MapFragment.java
@@ -4,13 +4,24 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import io.github.andikanugraha11.wherewego.R;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
 
     public MapFragment() {
         // Required empty public constructor
@@ -18,12 +29,28 @@ public class MapFragment extends Fragment {
 
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
-    }
 
+
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        SupportMapFragment fragment = new SupportMapFragment();
+        transaction.add(R.id.mapView, fragment);
+        transaction.commit();
+
+        fragment.getMapAsync(this);
+
+        return inflater.inflate(R.layout.fragment_map, container, false);
+
+    }
+    @Override
+    public void onMapReady(GoogleMap map) {
+        //map is ready
+
+    }
 
 }
